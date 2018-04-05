f80271a568fd3faadea2aea82bd8f7fd7b02b924
Ramy Salama <ramy.salama@spindriftgroup.com>
2018-04-05 14:14:39 +0200

    Update start-endeca.sh

--- config/start-endeca.sh
+++ config/start-endeca.sh
@@ -1,10 +1,10 @@
 # Start Endeca Servers
 
-/sde/endeca/CAS/11.3.0/bin/./cas-service.sh &  
+/OGS/endeca/CAS/11.3.0/bin/./cas-service.sh &  
 
-/sde/endeca/PlatformServices/11.3.0/tools/server/bin/./startup.sh 
+/OGS/endeca/PlatformServices/11.3.0/tools/server/bin/./startup.sh 
 
-/sde/endeca/ToolsAndFrameworks/11.3.0/server/bin/./startup.sh
+/OGS/endeca/ToolsAndFrameworks/11.3.0/server/bin/./startup.sh
 
 echo Endeca Services Started
 
