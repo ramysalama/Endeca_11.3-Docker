649f73baab3287bba6513f26b4b2a8e6518d43a9
Ramy Salama <ramy.salama@spindriftgroup.com>
2018-04-05 14:13:41 +0200

    Update shutdown-endeca.sh

--- config/shutdown-endeca.sh
+++ config/shutdown-endeca.sh
@@ -1,7 +1,7 @@
 # Shutdown Endeca Servers
 
-/sde/endeca/ToolsAndFrameworks/11.3.0/server/bin/./workbench.sh stop
-/sde/endeca/CAS/11.3.0/bin/./cas-service-shutdown.sh
-/sde/endeca/PlatformServices/11.3.0/tools/server/bin/./shutdown.sh
+/OGS/endeca/ToolsAndFrameworks/11.3.0/server/bin/./workbench.sh stop
+/OGS/endeca/CAS/11.3.0/bin/./cas-service-shutdown.sh
+/OGS/endeca/PlatformServices/11.3.0/tools/server/bin/./shutdown.sh
 
 echo Endeca Services shutdown.
